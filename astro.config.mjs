983f234e864e79aec51c1b62107db8df9148bb03
Pedro Moreira <pedrocsmoreira@gmail.com>
2026-02-08 00:43:30 +0000

    Add action for Astro build on push

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://links.pcsm.me',
   vite: {
     plugins: [tailwindcss()],
   },
